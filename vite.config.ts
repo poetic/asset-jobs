2d73de59da5e9f562afb969fad8f398372ee3809
Alan Fudge <alan@poetic.io>
2024-02-27 16:09:19 -0600

    setup initial cicd

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'index.js',
+        chunkFileNames: 'chunk.js',
+        assetFileNames: 'index.css',
+        manualChunks: undefined,
+      },
+    },
+  },
 });
